f98e5027d1fc3a035b1101ed1d96690096fcdbf0
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2024-01-27 10:19:22 -0300

    Some modifications...

--- inventory_control/src/App.js
+++ inventory_control/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import GlobalStyle from "./styles/global";
+import Header from "./components/Header/index";
+import Resume from "./components/Resume";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Resume />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
